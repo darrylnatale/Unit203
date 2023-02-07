787e330b72e4072eea872d76371a96e9965a5b73
darryl <darrylnatale@gmail.com>
2023-02-07 17:57:52 -0500

    remove hardcoded values and implement fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,141 +1,119 @@
-import styled from "styled-components"
-import CartItem from "./Components/CartItem";
-import { useState } from "react";
-const App = () => {
+  import styled from "styled-components"
+  import CartItem from "./Components/CartItem";
+  import { useState, useEffect } from "react";
+  const App = () => {
 
-  //Styling variables
-const BLUE = "#172162"; //"rgb(23, 33, 98)";
-const LIGHT_GREY = "#6e7484";
-const BLACK = "#000000";
+    //Styling variables
+  const BLUE = "#172162"; //"rgb(23, 33, 98)";
+  const LIGHT_GREY = "#6e7484";
+  const BLACK = "#000000";
 
-//First part given
-const [lineItems, setLineItems] = useState([
-{
-id: 1,
-title: "Grey Sofa",
-price: 499.99,
-quantity: 1,
-image: "https://www.cozey.ca/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0277%2F3057%2F5462%2Fproducts%2F2_Single_shot_DARK_GREY_OFF_OFF_SLOPE_17f0f115-11f8-4a78-b412-e9a2fea4748d.png%3Fv%3D1629310667&w=1920&q=75",
-swatchColor: "#959392",
-swatchTitle: "Grey"
-},
-{
-id: 2,
-title: "Blue Sofa",
-price: 994.99,
-quantity: 1,
-image: "https://www.cozey.ca/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0277%2F3057%2F5462%2Fproducts%2F3_Seater_SofaSofa_Ottoman_Off_Arm_Configuration_Two_Arms_Arm_Design_Slope_Chaise_Off_Fabric_Navy_Blue2.png%3Fv%3D1629231450&w=1920&q=75",
-swatchColor: "#191944",
-swatchTitle: "Blue"
+  //First part given
+  const [lineItems, setLineItems] = useState([]);
 
-},
-{
-id: 3,
-title: "White Sofa",
-price: 599.99,
-quantity: 1,
-image: "https://www.cozey.ca/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0277%2F3057%2F5462%2Fproducts%2F2_Single_shot_IVORY_OFF_OFF_SLOPE_5379af1f-9318-4e37-b514-962d33d1ce64.png%3Fv%3D1629231450&w=1920&q=75",
-swatchColor: "#F8F1EC",
-swatchTitle: "White"
-},
-]);
 
-const SUBTOTAL = 2094.97;
-const HST = 272.3461;
-const TOTAL = 2382.3161;
+  const removeLineItem = (lineItemId) => {
+    setLineItems(lineItems.filter(item => item.id !== lineItemId));
+  };
 
-const removeLineItem = (lineItemId) => {
-  setLineItems(lineItems.filter(item => item.id !== lineItemId));
-};
+  const addLineItem = (lineItem) => {
+    setLineItems([...lineItems, lineItem]);
+  };
 
-const addLineItem = (lineItem) => {
-  setLineItems([...lineItems, lineItem]);
-};
+  const calculateFees = (lineItems) => {
+    let subtotal = 0;
+    let tax = 0;
+    let shipping = 15;
 
-const calculateFees = (lineItems) => {
-  let subtotal = 0;
-  let tax = 0;
-  let shipping = 15;
+    lineItems.forEach((lineItem) => {
+      subtotal += lineItem.price * lineItem.quantity;
+    });
 
-  lineItems.forEach((lineItem) => {
-    subtotal += lineItem.price * lineItem.quantity;
-  });
+    tax = subtotal * 0.13;
+    const total = subtotal + tax + shipping;
 
-  tax = subtotal * 0.13;
-  const total = subtotal + tax + shipping;
+    return { subtotal, tax, shipping, total };
+  }
 
-  return { subtotal, tax, shipping, total };
-}
+  const fees = calculateFees(lineItems)
 
-const fees = calculateFees(lineItems)
+  useEffect(() => {
+    async function fetchLineItems() {
+      const res = await fetch("http://localhost:8000/lineItems");
+      const data = await res.json();
+      setLineItems(data.data);
+      console.log(data)
+    }
+    fetchLineItems();
+  }, []);
 
-return (<>
-  <CartContainer>
-      <CartTitle>Your Cart</CartTitle>
-      
-      {lineItems.map(item => (
-        <CartItem key={item.id} item={item} removeLineItem={removeLineItem}/>
-      ))}
-      
-    </CartContainer>
-    <button onClick={() => addLineItem({ name: 'item', price: 10.99, quantity: 1})}>
-        Add Item
-      </button>
-    <FeeContainer>
-      <PriceCategory>
-        <Label>Subtotal</Label>
-        <Value>{fees.subtotal.toFixed(2)}</Value>
-      </PriceCategory>
-      <PriceCategory>
-        <Label>Taxes (estimated)</Label>
-        <Value>{fees.tax.toFixed(2)}</Value>
-      </PriceCategory>
-      <PriceCategory>
-        <Label>Shipping</Label>
-        <Value>{fees.shipping.toFixed(2)}</Value>
-      </PriceCategory>
-      <PriceCategory>
-        <Label>Total</Label>
-        <Value>{fees.total.toFixed(2)}</Value>
-      </PriceCategory>
-    </FeeContainer>
-  </>
-);
+  return (<>
+    <CartContainer>
+        <CartTitle>Your Cart</CartTitle>
+        
+        {lineItems.map(item => (
+          <CartItem key={item.id} item={item} removeLineItem={removeLineItem}/>
+        ))}
+        
+      </CartContainer>
+      <button onClick={() => addLineItem({ name: 'item', price: 10.99, quantity: 1})}>
+          Add Item
+        </button>
+      <FeeContainer>
+        <PriceCategory>
+          <Label>Subtotal</Label>
+          <Value>{fees.subtotal.toFixed(2)}</Value>
+        </PriceCategory>
+        <PriceCategory>
+          <Label>Taxes (estimated)</Label>
+          <Value>{fees.tax.toFixed(2)}</Value>
+        </PriceCategory>
+        <PriceCategory>
+          <Label>Shipping</Label>
+          <Value>{fees.shipping.toFixed(2)}</Value>
+        </PriceCategory>
+        <PriceCategory>
+          <Label>Total</Label>
+          <Value>{fees.total.toFixed(2)}</Value>
+        </PriceCategory>
+      </FeeContainer>
+    </>
+  );
 
-}
+  }
 
-export default App;
+  export default App;
 
-const CartContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+  const CartContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
 
-const CartTitle = styled.div`
-  font-size: 20px;
-  font-weight: bold;
-  margin-bottom: 10px;
-`;
+  const CartTitle = styled.div`
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  `;
 
-const FeeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  padding: 16px;
-`;
+  const FeeContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 16px;
+  `;
 
-const PriceCategory = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin-bottom: 8px;
-`;
+  const PriceCategory = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-bottom: 8px;
+  `;
 
-const Label = styled.div`
-  font-weight: bold;
-  text-align: left;
-`;
+  const Label = styled.div`
+    font-weight: bold;
+    text-align: left;
+  `;
 
-const Value = styled.div`
-  text-align: right;
-`;
+  const Value = styled.div`
+    text-align: right;
+  `;
